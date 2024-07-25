fe4e93d65c9876a0de691e2287790c05f8e9b7b9
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2024-07-25 11:50:38 +0600

    Add files via upload

---
+++ Conditional.statement/Conditional.statement/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Conditional Shortcut");
+        int age = 10;
+        String Vote = (age >= 18)?"Yes":"No";
+        System.out.println(Vote);
+    }
+}
+
