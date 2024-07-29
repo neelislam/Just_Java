e7e8ee54c1d031122882ae29ba423cb262597e87
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2024-07-29 23:45:13 +0600

    Add files via upload

---
+++ Try and catch/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+int[] marks =  {97,98,95};
+try{
+System.out.println(marks[5]);
+    } catch(Exception exception){
+
+}
+System.out.println("An Exception is handled above...!");
+        }
+    }
